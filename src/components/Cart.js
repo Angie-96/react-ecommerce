81229284696fac8c3f9da5a3634c7f2f2efc862b
Angie <angie@genosha.com.ar>
2021-03-21 18:25:17 -0300

    Add empty cart message

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -5,7 +5,11 @@ import { CartItem } from "./CartItem";
 
 const useStyles = makeStyles({
   root: {
-    backgroundColor: "#e8e8e8",
+    "& p": {
+      textAlign: "center",
+      backgroundColor: "transparent",
+      fontFamily: "Lato, sans-serif",
+    },
     maxWidth: "90%",
     margin: "20px auto 0 auto",
   },
@@ -15,11 +19,30 @@ export const Cart = () => {
   const classes = useStyles();
   const { cartProducts } = useContext(CartContext);
 
+  const cartProductsGroupById = cartProducts.reduce(function (
+    groupedProducts,
+    product
+  ) {
+    (groupedProducts[product.id] = groupedProducts[product.id] || []).push(
+      product
+    );
+    return groupedProducts;
+  },
+  []);
+
   return (
     <div className={classes.root}>
-      {cartProducts.map((cartProduct) => (
-        <CartItem key={cartProduct.id} {...cartProduct} />
-      ))}
+      {cartProductsGroupById.length ? (
+        cartProductsGroupById.map((cartProduct) => (
+          <CartItem
+            key={cartProduct[0].id}
+            {...cartProduct[0]}
+            value={cartProduct.length}
+          />
+        ))
+      ) : (
+        <p>Your cart is empty</p>
+      )}
     </div>
   );
 };
